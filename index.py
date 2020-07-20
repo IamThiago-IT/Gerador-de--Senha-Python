d2c298f9056744305b7f950a58a550f5109d2114
IamThiago-IT <thiagodossantos315@gmail.com>
2020-07-20 12:20:43 -0300

    Update index.py

--- index.py
+++ index.py
@@ -5,3 +5,5 @@ print("{:^}".format("Gerador de senhas"))
 print("#"*30)
 while True:
   print('{:^30}'.format('A senha gerada é:'))
+    for i in range(10):
+      dic = choice([1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", 
