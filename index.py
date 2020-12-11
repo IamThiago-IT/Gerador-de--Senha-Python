15589fbb9558195148e0437a183fc82ba5266eb8
IamThiago-IT <thiagodossantos315@gmail.com>
2020-12-11 09:18:34 -0300

    Delete index.py

--- index.py
+++
@@ -1,9 +0,0 @@
-#gerador de senhas
-from random import choice
-print('#'*30)
-print("{:^}".format("Gerador de senhas"))
-print("#"*30)
-while True:
-  print('{:^30}'.format('A senha gerada é:'))
-    for i in range(10):
-      dic = choice([1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", 
